fbfcfd0f5a8242d0cd76bb7afff55f7d58434a7d
UladzimirKurchych <kurchich.v@gmail.com>
2023-11-09 20:25:53 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
 
 function App() {
   return (
     <div className="App">
-      My portfolio
+      <Box>My portfolio</Box>
     </div>
   );
 }
 
 export default App;
+
+const Box = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  font-weight: bold;
+  background-color: chartreuse;
+  color: #282c34;
+`
